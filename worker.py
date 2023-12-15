44d15ab5d6db857e9a764fa34991a270b13cfc18
DemiAina <100835583+DemiAina@users.noreply.github.com>
2023-12-15 00:07:24 +0000

    fix

--- worker.py
+++ worker.py
@@ -35,7 +35,7 @@ def add():
 def addWorker(token, num):
     with open('payload.json') as p:
       tdata=json.load(p)
-    tdata['name']='sparkplug-master-'+str(num)
+    tdata['name']=f"sparkplug-master-{num}"
     data=json.dumps(tdata)
     url='https://www.googleapis.com/compute/v1/projects/lab5-407311/zones/europe-west1-b/instances'
     headers={"Authorization": "Bearer "+token}
